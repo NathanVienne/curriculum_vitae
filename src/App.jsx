159de686b3d3d073a8c2845e04f77b560cae1a6c
Nathan Vienne <nathan.vienne@outlook.fr>
2025-10-02 20:34:24 +0400

    About part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navigation from "./components/nav/nav"
 import Hero from "./components/hero"
+import About from "./components/about"
 function App() {
 
   return (
     <>
-      <Navigation/>
-      <Hero/>
+      <Navigation />
+      <Hero />
+      <About />
     </>
   )
 }
---
+++ src/components/about.jsx
@@ -0,0 +1,24 @@
+export default function About() {
+    return (
+        <section className="bg-neutral text-white">
+            <div className="text-center">
+                <h2 className="pt-10 pb-5 text-4xl font-bold">A Propos</h2>
+                <p className="text-xl pb-15 text-accent">En savoir plus sur moi & mes valeurs</p>
+            </div>
+            <div className="p-15 bg-primary mx-40 rounded-2xl border border-info space-y-5 text-justify text-lg">
+                <p>Passionné par le développement web depuis plus de 5 ans, je m'efforce de créer des solutions élégantes et performantes qui répondent aux besoins réels des utilisateurs.
+                    Mon approche combine expertise technique, créativité et souci du détail pour livrer des produits de qualité.</p>
+                    <p>Je crois fermement en l'apprentissage continu et j'aime explorer de nouvelles technologies.
+                    La collaboration et le partage de connaissances sont au cœur de ma philosophie professionnelle.</p>
+                    <p>En dehors du code, j'apprécie les moments passés à découvrir de nouveaux lieux, écouter de la musique et contribuer à des projets open source.
+                    Je suis toujours ouvert à de nouvelles opportunités et collaborations enrichissantes.</p>
+            </div>
+            <div>
+                <h3>Centres d'Intérêts</h3>
+                <div>
+
+                </div>
+            </div>
+        </section>
+    )
+}
