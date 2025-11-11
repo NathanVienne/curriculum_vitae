625d9af04b000e35a4d3fd9bee18d404e2a5348c
Nathan Vienne <nathan.vienne@outlook.fr>
2025-11-11 10:58:43 +0400

    CV's path udpated for download

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -30,7 +30,7 @@ function Hero() {
                                 <div className="btn btn-sm md:btn-md btn-primary border-neutral hover:btn-accent hover:text-primary">
                                     <Icon path={mdiTrayArrowDown} size={1} />
                                     <a href="/public/CV-VIENNE_Nathan.pdf"
-                                        download={CV}>Télécharger CV</a>
+                                        download={`${import.meta.env.BASE_URL}/${CV}`}>Télécharger CV</a>
                                 </div>
                                 <div className="btn btn-sm md:btn-md btn-primary border-neutral hover:btn-secondary hover:text-primary">
                                     <Icon path={mdiEmailOutline} size={1} />
